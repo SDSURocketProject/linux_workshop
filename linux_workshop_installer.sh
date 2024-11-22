a7a58531794c467b23e7fab7b4066c9120a858dc
Athena Shikata <evanperes7@gmail.com>
2024-11-21 17:00:56 -0800

    fix

--- linux_workshop_installer.sh
+++ linux_workshop_installer.sh
@@ -5,8 +5,8 @@ sudo apt-get autoremove -y
 curl -fsSL https://tailscale.com/install.sh | sh
 curl -sSL https://get.docker.com | sh
 sudo apt-get install docker-compose -y
-wget https://raw.githubusercontent.com/AthenaShikata/mySDSURocketProject/refs/heads/main/2024-04-20_Linux_Workshop/linux_workshop1.yaml
-wget https://raw.githubusercontent.com/AthenaShikata/mySDSURocketProject/refs/heads/main/2024-04-20_Linux_Workshop/linux_workshop2.yaml
+wget https://raw.githubusercontent.com/SDSURocketProject/linux_workshop/refs/heads/master/linux_workshop1.yaml
+wget https://raw.githubusercontent.com/SDSURocketProject/linux_workshop/refs/heads/master/linux_workshop2.yaml
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y

f14d0092948167baae7d7aaaf793ba9f69249829
EvanOfTheAces <evanperes7@gmail.com>
2025-01-13 15:51:30 -0800

    Update linux_workshop_installer.sh

--- linux_workshop_installer.sh
+++ linux_workshop_installer.sh
@@ -3,14 +3,20 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 curl -fsSL https://tailscale.com/install.sh | sh
-curl -sSL https://get.docker.com | sh
-sudo apt-get install docker-compose -y
-sudo docker image pull httpd:latest
-sudo docker image pull jc21/nginx-proxy-manager:latest
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 wget https://raw.githubusercontent.com/SDSURocketProject/linux_workshop/refs/heads/master/linux_workshop1.yaml
 wget https://raw.githubusercontent.com/SDSURocketProject/linux_workshop/refs/heads/master/linux_workshop2.yaml
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
+sudo docker image pull httpd:latest
+sudo docker image pull jc21/nginx-proxy-manager:latest
 sudo tailscale up
-read -p "- Press Return to exit..." _
